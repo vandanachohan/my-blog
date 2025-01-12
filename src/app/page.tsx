eed8cf8763454fdb33ba9ef1607e0794407ff13f
vandanachohan <161231376+vandanachohan@users.noreply.github.com>
2025-01-12 18:29:28 +0500

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,4 @@
-// import { groq } from "next-sanity"; // Corrected syntax
-// import Hero from "./components/hero";
-// import { client } from "@/sanity/lib/client";
-// import BlogContent from "./components/blogcontent";
 
-// // Execute GROQ Query
-// const query = groq`
-//   *[_type == 'post']{
-//     ...,
-//     author->,
-//     categories[]->
-//   } | order(_createdAt asc)
-// `;
-
-// export default async function Home() {
-//   // Fetch data from Sanity
-//   const posts = await client.fetch(query);
-// console.log(posts); // Check if posts array has more than one post
-
-
-//   return (
-//     <main className="container mx-auto p-4">
-//       <Hero />
-      
-//     <BlogContent posts={posts} />
-      
-      
-//     </main>
-//   );
-// }
 import { groq } from "next-sanity";
 import Hero from "./components/hero";
 import { client } from "@/sanity/lib/client";
